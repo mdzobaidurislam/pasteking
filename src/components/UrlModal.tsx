64706e539080da962153f1c4ce63b17c462ea7c5
jamiislam <md.zobaidurislam@gmail.com>
2024-11-07 16:04:50 +0600

    live

--- src/components/UrlModal.tsx
+++ src/components/UrlModal.tsx
@@ -8,11 +8,11 @@ interface UrlModalProps {
 }
 
 const UrlModal: React.FC<UrlModalProps> = ({ newUrl, setNewUrl }) => {
-
+    const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
     const handleCopyToClipboard = async () => {
         try {
-            await navigator.clipboard.writeText("localhost:3000/" + newUrl);
+            await navigator.clipboard.writeText(NEXT_PUBLIC_BASE_URL + "/" + newUrl);
             alert("Text copied to clipboard!");
         } catch (err) {
             console.error("Failed to copy: ", err);
@@ -22,7 +22,7 @@ const UrlModal: React.FC<UrlModalProps> = ({ newUrl, setNewUrl }) => {
     return (
         <div className="bg-[#00000063] fixed top-0 left-0 w-[100vw] h-[100vh]">
             <div className="bg-[#ff3c87d0] rounded-lg px-3 fixed relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[40%] h-[15%] flex justify-center items-center">
-                <input type="text" value={"localhost:3000/" + newUrl} className="outline-none rounded-s-md text-black w-[80%] p-2 text-center" /><button onClick={handleCopyToClipboard} className="rounded-e-md  w-[10%] p-2 text-center bg-gray-800">copy</button>
+                <input type="text" value={NEXT_PUBLIC_BASE_URL + "/" + newUrl} className="outline-none rounded-s-md text-black w-[80%] p-2 text-center" /><button onClick={handleCopyToClipboard} className="rounded-e-md  w-[10%] p-2 text-center bg-gray-800">copy</button>
                 <span className="absolute top-1 right-4 text-gray-700 text-xl cursor-default" onClick={() => setNewUrl("")}>x</span>
             </div>
         </div>
